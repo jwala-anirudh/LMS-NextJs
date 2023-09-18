b5722a06bce9dc303eb890f81d1635f3ffa51120
Anirudh Jwala <anirudhsai2@gmail.com>
2023-09-18 23:01:08 +0530

    Application layout with sidebar, navigation section, reusable side action items, action sheet for mobile responsive design

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image';
+
+const Logo = () => {
+  return <Image height={130} width={130} alt="LMS" src="./logo.svg" />;
+};
+
+export default Logo;
---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Layout, Compass } from 'lucide-react';
+import SidebarItem from './sidebar-item';
+
+const guestRotues = [
+  {
+    icon: Layout,
+    label: 'Dashboard',
+    href: '/',
+  },
+  {
+    icon: Compass,
+    label: 'Browse',
+    href: '/search',
+  },
+];
+
+const SidebarRoutes = () => {
+  const routes = guestRotues;
+
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SidebarRoutes;
