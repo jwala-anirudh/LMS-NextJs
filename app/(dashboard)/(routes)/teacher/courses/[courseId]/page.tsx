37bb07be70c422ce09940dc83cb395b6d241fea6
Anirudh Jwala <anirudhsai2@gmail.com>
2023-09-19 22:38:13 +0530

    Course category for teacher to edit and update

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import { IconBadge } from '@/components/icon-badge';
 import TitleForm from './_components/title-form';
 import DescriptionForm from './_components/description-form';
 import ImageForm from './_components/image-form';
+import CategoryForm from './_components/category-form';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   // To verify if the course creator, is the one editing it
@@ -22,6 +23,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+    },
+  });
+
+  // Query to database to load the seeded categories list
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: 'asc',
     },
   });
 
@@ -62,6 +70,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <TitleForm initialData={course} courseId={course.id} />
           <DescriptionForm initialData={course} courseId={course.id} />
           <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
         </div>
       </div>
     </div>
---
+++ scripts/seed.ts
@@ -0,0 +1,26 @@
+const { PrismaClient } = require('@prisma/client');
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: 'Computer Science' },
+        { name: 'Music' },
+        { name: 'Fitness' },
+        { name: 'Accounting' },
+        { name: 'Photography' },
+        { name: 'Filming' },
+      ],
+    });
+
+    console.log('Success seeding categories');
+  } catch (error) {
+    console.log('Error seeding the database categories', error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main();
