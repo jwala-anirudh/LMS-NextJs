ad6269cef0e23213d0ba5ee507e5f36473dc5d73
Anirudh Jwala <anirudhsai2@gmail.com>
2023-09-19 19:42:46 +0530

    Course title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { LayoutDashboard } from 'lucide-react';
 
 import { db } from '@/lib/db';
 import { IconBadge } from '@/components/icon-badge';
+import TitleForm from './_components/title-form';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   // To verify if the course creator, is the one editing it
@@ -54,6 +55,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             <IconBadge icon={LayoutDashboard} />
             <h2 className="text-xl">Customize your course</h2>
           </div>
+          <TitleForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
