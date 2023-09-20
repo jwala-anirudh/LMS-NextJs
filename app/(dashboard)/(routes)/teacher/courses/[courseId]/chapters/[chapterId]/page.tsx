fc070bd3405f68d1416283e3bf72b8980bb29aa1
Anirudh Jwala <anirudhsai2@gmail.com>
2023-09-21 00:39:19 +0530

    Mux video + video player: Chapter video player upload

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -11,6 +11,7 @@ import { IconBadge } from '@/components/icon-badge';
 import ChapterTitleForm from './_components/chapter-title-form';
 import ChapterDescriptionForm from './_components/chapter-description-form';
 import ChapterAccessForm from './_components/chapter-access-form';
+import ChapterVideoForm from './_components/chapter-video-form';
 
 const ChapterIdPage = async ({
   params,
@@ -90,7 +91,7 @@ const ChapterIdPage = async ({
             </div>
             <ChapterAccessForm
               initialData={chapter}
-              courseId={params.chapterId}
+              courseId={params.courseId}
               chapterId={params.chapterId}
             />
           </div>
@@ -100,6 +101,11 @@ const ChapterIdPage = async ({
             <IconBadge icon={Video} />
             <h2 className="text-xl">Add a video</h2>
           </div>
+          <ChapterVideoForm
+            initialData={chapter}
+            courseId={params.courseId}
+            chapterId={params.chapterId}
+          />
         </div>
       </div>
     </div>
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from 'next/server';
+
 import { auth } from '@clerk/nextjs';
+import Mux from '@mux/mux-node';
 
 import { db } from '@/lib/db';
+
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKEN_SECRET!
+);
 
 export async function PATCH(
   req: Request,
@@ -36,7 +43,36 @@ export async function PATCH(
       },
     });
 
-    // TODO: Handle video upload
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+
+      const asset = await Video.Assets.create({
+        input: values.videoUrl,
+        playback_policy: 'public',
+        test: false,
+      });
+
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0].id,
+        },
+      });
+    }
 
     return NextResponse.json(chapter);
   } catch (error) {
