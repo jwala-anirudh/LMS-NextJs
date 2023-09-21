643407084124a03ffd1663907ec21fe86ae70a3f
Anirudh Jwala <anirudhsai2@gmail.com>
2023-09-21 13:04:50 +0530

    Courses data table with sorting, filtering and pagination

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,35 @@
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { redirect } from 'next/navigation';
 
-const CoursesPage = () => {
+import { auth } from '@clerk/nextjs';
+
+// Components
+import { Button } from '@/components/ui/button';
+import { DataTable } from './_components/data-table';
+import { columns } from './_components/columns';
+
+// Database
+import { db } from '@/lib/db';
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect('/');
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
