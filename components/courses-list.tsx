f2b4ff857146a28456fd4251ecbc1a239d51daad
Anirudh Jwala <anirudhsai2@gmail.com>
2023-09-21 15:42:03 +0530

    Courses grid & chapters

---
+++ components/courses-list.tsx
@@ -0,0 +1,39 @@
+import { Category, Course } from '@prisma/client';
+
+import { CourseCard } from '@/components/course-card';
+
+type CourseWithProgressCategory = Course & {
+  category: Category | null;
+  chapters: { id: string }[];
+  progress: number | null;
+};
+
+interface CoursesListProps {
+  items: CourseWithProgressCategory[];
+}
+
+export const CoursesList = ({ items }: CoursesListProps) => {
+  return (
+    <div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+        {items.map((item) => (
+          <CourseCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            imageUrl={item.imageUrl!}
+            chaptersLength={item.chapters.length}
+            price={item.price!}
+            progress={item.progress}
+            category={item?.category?.name!}
+          />
+        ))}
+      </div>
+      {items.length === 0 && (
+        <div className="text-center text-sm text-muted-foreground mt-10">
+          No courses found
+        </div>
+      )}
+    </div>
+  );
+};
