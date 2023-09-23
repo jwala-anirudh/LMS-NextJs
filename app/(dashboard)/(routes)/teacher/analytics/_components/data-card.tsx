7dd5bb8d326b767a09d0fe4f40d57c691273e6d2
Anirudh Jwala <anirudhsai2@gmail.com>
2023-09-23 17:08:35 +0530

    Admin analytics

---
+++ app/(dashboard)/(routes)/teacher/analytics/_components/data-card.tsx
@@ -0,0 +1,24 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+import { formatPrice } from '@/lib/format';
+
+interface DataCardProps {
+  value: number;
+  label: string;
+  shouldFormat?: boolean;
+}
+
+export const DataCard = ({ label, value, shouldFormat }: DataCardProps) => {
+  return (
+    <Card>
+      <CardHeader className="flex felx-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">{label}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="2-xl font-bold">
+          {shouldFormat ? formatPrice(value) : value}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,34 @@
-const AnalyticsPage = () => {
-  return <div>this is analytics page</div>;
+import { redirect } from 'next/navigation';
+
+import { auth } from '@clerk/nextjs';
+
+import { DataCard } from './_components/data-card';
+import { Chart } from './_components/chart';
+
+import { getAnalytics } from '@/actions/get-analytics';
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect('/');
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard
+          label="Total Revenue"
+          value={totalRevenue}
+          shouldFormat={true}
+        />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+      <Chart data={data} />
+    </div>
+  );
 };
 
 export default AnalyticsPage;
